31cb8ac8badc7aef76a2195529d441840578f598
John Payne <jjrpayne@hotmail.com>
2023-03-26 19:46:00 -0700

    made card formatting more uniform

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,8 +20,10 @@ export default function Home() {
 							<div className="row">
 								<div className="col-md-6">
 									<div className="row">
-										<div className="col-md-12 bg-pastelblue">
-											<h1><b>About Me</b></h1>
+										<div className="col-md-12">
+											<div className="card-jj bg-pastelblue">
+												<h1><b>About Me</b></h1>
+											</div>
 										</div>
 									</div>
 									<div className="row">
@@ -30,16 +32,18 @@ export default function Home() {
 										</div>
 									</div>
 									<div className="row">
-										<div className="col-md-12 bg-white">
-											<p>Hello! My name is John Payne.</p>
-											<p>I graduated from UVic's computer science program in 2021 and since then I've worked as a full-stack developer on web and mobile projects.</p>
-											<p>Outside of work, some of my hobbies include riding my bike, going on nature walks, and playing the guitar and piano.</p>
-											<p>To read more about my work experience and some of my favourite personal projects, feel free to keep scrolling!</p>
+										<div className="col-md-12">
+											<div className="card-jj bg-white">
+												<p>Hello! My name is John Payne.</p>
+												<p>I graduated from UVic's computer science program in 2021 and since then I've worked as a full-stack developer on web and mobile projects.</p>
+												<p>Outside of work, some of my hobbies include riding my bike, going on nature walks, and playing the guitar and piano.</p>
+												<p>To read more about my work experience and some of my favourite personal projects, feel free to keep scrolling!</p>
+											</div>
 										</div>
 									</div>
 								</div>
 								<div className="col-md-6">
-									<img className="img-responsive lazy" loading="lazy" width="90%" src="https://res.cloudinary.com/di2iadfzv/image/upload/v1677881036/me_3_pabr2a.jpg" alt="photo of John Payne"/>
+									<img className="img-responsive lazy card-image" loading="lazy" width="100%" src="https://res.cloudinary.com/di2iadfzv/image/upload/v1677881036/me_3_pabr2a.jpg" alt="photo of John Payne"/>
 								</div>
 							</div>
 						</div>
@@ -47,58 +51,57 @@ export default function Home() {
 					<div className="index-section" id="projects">
 						<div className="container">
 							<div className="row">
-								<div className="col-md-12 bg-pastelgreen">
-									<h1><b>Selected Projects</b></h1>
-								</div>
-							</div>
-							<div className="row">
 								<div className="col-md-12">
-									<br />
-								</div>
-							</div>
-							<div className="row">
-								<div className="col-md-12 bg-white">
-									<p>These are some of my favourite personal projects that I've worked on.</p>
-								</div>
-							</div>
-							<div className="row top-bottom-padding">
-								<div className="col-md-5 bg-white">
-									<h2 className="text-jjgreen"><b>Hatchways Projects</b></h2>
-									<p>This is a selection of projects I completed for the company Hatchways.</p>
-									<h3 className="text-jjgreen">Hackathon</h3>
-									<p>Implemented new endpoints and SQL queries in a Javascript API as part of my submission to their March 2023 hackathon.</p>
-									<h3 className="text-jjgreen">Assessments</h3>
-									<p>These are assessments I performed in order to test them for the company.</p>
-									<p>• Ruby on Rails Assessment, January 2023</p>
-									<p>• Formula Interpreter Exercises, Python, November 2022</p>
-									<p>All of the projects I've done for Hatchways are on private repositories, please <Link class="scroll-link" to="/#contact">contact me</Link> to learn more.</p>
-								</div>
-								<div className="col-md-6 offset-1">
-									<div className="row">
-										<div className="col-md-12 bg-white">
-											<h2 className="text-jjgreen"><b>Random Episode Picker</b></h2>
-											<p>Web application for selecting a random episode of a given TV show. Developed with a React Frontend and a NodeJS pass-through service for the OMDb API.</p>
-											<p>Links: <a href="https://github.com/jjrpayne/random-episode-picker" target="_blank">GitHub</a>, <a href= "https://random-episode-picker.netlify.app/"target="_blank">Web App</a></p>
-										</div>
-									</div>
-									<div className="row top-bottom-padding">
-										<div className="col-md-12">
-											<img className="img-responsive lazy image-fullwidth" width="100%" loading="lazy" src="https://res.cloudinary.com/di2iadfzv/image/upload/v1678490653/Screenshot_from_2023-03-10_15-21-03_efbotb.png" alt="Screenshot of Random Episode Picker App"/>
-										</div>
+									<div className="card-jj bg-pastelgreen">
+										<h1><b>Selected Projects</b></h1>
 									</div>
 								</div>
 							</div>
 							<div className="row top-bottom-padding">
 								<div className="col-md-6">
-								<img className="img-responsive lazy" loading="lazy" width="90%" src="https://res.cloudinary.com/di2iadfzv/image/upload/v1679263668/spacedash-screenshot_ke4eu7.png" alt="screenshot of Space Dash game"/>									
+									<div className="card-jj bg-white">
+										<h2 className="text-jjgreen"><b>Hatchways Projects</b></h2>
+										<p>This is a selection of projects I completed for the company Hatchways.</p>
+										<h3 className="text-jjgreen">Hackathon</h3>
+										<p>Implemented new endpoints and SQL queries in a Javascript API as part of my submission to their March 2023 hackathon.</p>
+										<h3 className="text-jjgreen">Assessments</h3>
+										<p>These are assessments I performed in order to test them for the company.</p>
+										<p>• Ruby on Rails Assessment, January 2023</p>
+										<p>• Formula Interpreter Exercises, Python, November 2022</p>
+										<p>All of the projects I've done for Hatchways are on private repositories, please <Link class="scroll-link" to="/#contact">contact me</Link> to learn more.</p>
+									</div>
 								</div>
 								<div className="col-md-6">
-									<div className="row">
-										<div className="col-md-12 bg-white">
-											<h2 className="text-jjgreen"><b>Space Dash</b></h2>
-											<p>A cross-platform game developed using Java and the libGDX framework.</p>
-											<p>Links: <a href="https://github.com/jjrpayne/spacedash" target="_blank">GitHub</a>, <a href= "https://spacedash.netlify.app/"target="_blank">Web App</a>, <a href= "https://play.google.com/store/apps/details?id=com.gamehut.spacedash"target="_blank">Google Play</a></p>
-										</div>
+									<div card-image>
+										<img className="img-responsive lazy card-image" loading="lazy" width="100%" src="https://res.cloudinary.com/di2iadfzv/image/upload/v1679883315/stock_warning_screenshot_wodv4u.png" alt="output of endpoint implemened for hatchways hackathon"/>
+									</div>
+								</div>
+							</div>
+							<div className="row top-bottom-padding">
+								<div className="col-md-6">
+									<div className="card-image">
+										<img className="img-responsive lazy" width="100%" loading="lazy" src="https://res.cloudinary.com/di2iadfzv/image/upload/v1678490653/Screenshot_from_2023-03-10_15-21-03_efbotb.png" alt="Screenshot of Random Episode Picker App"/>
+									</div>
+								</div>
+								<div className="col-md-6">
+									<div className="card-jj bg-white">
+										<h2 className="text-jjgreen"><b>Random Episode Picker</b></h2>
+										<p>Web application for selecting a random episode of a given TV show. Developed with a React Frontend and a NodeJS pass-through service for the OMDb API.</p>
+										<p>Links: <a href="https://github.com/jjrpayne/random-episode-picker" target="_blank">GitHub</a>, <a href= "https://random-episode-picker.netlify.app/"target="_blank">Web App</a></p>
+									</div>
+								</div>
+							</div>
+							<div className="row top-bottom-padding">
+								<div className="col-md-6">
+									<div className="card-jj bg-white">
+										<h2 className="text-jjgreen"><b>Space Dash</b></h2>
+										<p>A cross-platform game developed using Java and the libGDX framework.</p>
+										<p>Links: <a href="https://github.com/jjrpayne/spacedash" target="_blank">GitHub</a>, <a href= "https://spacedash.netlify.app/"target="_blank">Web App</a>, <a href= "https://play.google.com/store/apps/details?id=com.gamehut.spacedash"target="_blank">Google Play</a></p>									
+									</div>
+								</div>
+								<div className="col-md-6">
+									<div className="card-image">
+										<img className="img-responsive lazy" loading="lazy" width="100%" src="https://res.cloudinary.com/di2iadfzv/image/upload/v1679263668/spacedash-screenshot_ke4eu7.png" alt="screenshot of Space Dash game"/>
 									</div>
 								</div>
 							</div>
@@ -107,18 +110,24 @@ export default function Home() {
 					<div className="index-section" id="experience">
 						<div className="container">
 							<div className="row top-bottom-padding">
-								<div className="col-md-12 bg-pastelred">
-									<h1><b>Work Experience</b></h1>
+								<div className="col-md-12">
+									<div className="card-jj bg-pastelred">
+										<h1><b>Work Experience</b></h1>
+									</div>
 								</div>
 							</div>
 							<div className="row top-bottom-padding">
-								<div className="col-md-5 bg-white">
-									<h2 className="text-jjred"><b>AOT Technologies</b></h2>
-									<p>This section is under construction.</p>
+								<div className="col-md-6">
+									<div className="card-jj bg-white">
+										<h2 className="text-jjred"><b>AOT Technologies</b></h2>
+										<p>This section is under construction.</p>
+									</div>
 								</div>
-								<div className="col-md-5 offset-1 bg-white">
-									<h2 className="text-jjred"><b>Salyx Medical</b></h2>
-									<p>This section is under construction</p>
+								<div className="col-md-6">
+									<div className="card-jj bg-white">
+										<h2 className="text-jjred"><b>Salyx Medical</b></h2>
+										<p>This section is under construction</p>
+									</div>
 								</div>
 							</div>
 						</div>
@@ -126,13 +135,17 @@ export default function Home() {
 					<div className="index-section contact-section" id="contact">
 						<div className="container">
 							<div className="row top-bottom-padding">
-								<div className="col-md-12 bg-pastelpurple">
-									<h1><b>Contact Me</b></h1>
+								<div className="col-md-12">
+									<div className="card-jj bg-pastelpurple">
+										<h1><b>Contact Me</b></h1>
+									</div>
 								</div>
 							</div>
 							<div className="row top-bottom-padding">
-								<div className="col-md-12 bg-white">
-									<p>This section is under construction.</p>
+								<div className="col-md-12">
+									<div className="card-jj bg-white">
+										<p>This section is under construction.</p>
+									</div>
 								</div>
 							</div>
 						</div>
